6331de3673dce9b26f313bbfdf342714c267f591
NikolayPL <Eaxaxeaexdqw7>
2019-03-02 10:47:13 +0200

    change text color logic

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -29,15 +20,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -49,31 +31,37 @@ class _MyHomePageState extends State<MyHomePage> {
   final Random _random = Random();
 
   Color _color = Color(0xFFFFFFFF);
+  Color _colorText;
 
   void changeColor() {
+    Color color = Color.fromARGB(
+      _random.nextInt(256),
+      _random.nextInt(256),
+      _random.nextInt(256),
+      _random.nextInt(256),
+    );
+
+    Color textColor;
+
+    if (color.red > 150 || color.green > 150 || color.blue > 150) {
+      textColor = Colors.white;
+    } else {
+
+      textColor = Colors.black;
+    }
+
     setState(() {
-      _color = Color.fromARGB(
-        //or with fromRGBO with fourth arg as _random.nextDouble(),
-        _random.nextInt(256),
-        _random.nextInt(256),
-        _random.nextInt(256),
-        _random.nextInt(256),
-      );
+      _color = color;
+      _colorText = textColor;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return GestureDetector(
         onTap: () {
           setState(() {
-            changeColor();
+            changeColor()
           });
         },
         child: Scaffold(
@@ -85,6 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
             alignment: Alignment.center,
             child: new Text(
               "Hey there",
+              style: new TextStyle(color: _colorText),
               textAlign: TextAlign.center,
             ),
           ),
